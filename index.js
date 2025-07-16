2a1a30f4ab712e94d28c549d8223076f8645a1df
Nityananda Krishnamoorthy <87063077+Nityananda-Krishnamoorthy@users.noreply.github.com>
2025-07-16 17:51:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,33 +1,18 @@
-// Generate a random number between 1 and 6
-var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 
-// Construct the file name of the dice image
-var randomDiceImage = 'images/dice' + randomNumber1 + '.png';
+function rollDice() {
+    randomNumber1 = Math.floor(Math.random() * 6) + 1; // 1-6
+    randomNumber2 = Math.floor(Math.random() * 6) + 1; // 1-6
+    document.getElementsByClassName("img1")[0].setAttribute("src", "images/dice" + randomNumber1 + ".png");
+    document.getElementsByClassName("img2")[0].setAttribute("src", "images/dice" + randomNumber2 + ".png");
+    if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "🚩 Player 1 Wins!";
+    }   else if (randomNumber2 > randomNumber1) {
+    document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
+    }
+else {
+    document.querySelector("h1").innerHTML = "Draw!";
+    document.querySelector("h1").style.color = "red"; // Change text color to red
+}
+}
+document.querySelector(".btn").addEventListener("click", rollDice);
 
-// Select the left <img> element with class img1
-var leftDiceImage = document.querySelector('.img1');
-
-// Set the src attribute to the new image
-leftDiceImage.src = randomDiceImage;
-
-// Generate a random number between 1 and 6
-var randomNumber2 = Math.floor(Math.random() * 6) + 1;
-
-// Construct the file name of the dice image
-var randomDiceImage = 'images/dice' + randomNumber2 + '.png';
-
-// Select the left <img> element with class img1
-var rightDiceImage = document.querySelector('.img2');
-
-// Set the src attribute to the new image
-rightDiceImage.src = randomDiceImage;
-
-var heading = document.querySelector("h1");
-
-if(randomNumber1 > randomNumber2) {
-    heading.textContent = "Player 1 Wins!";
-    } else if(randomNumber2 > randomNumber1) {
-        heading.textContent = "Player 2 Wins!";
-        } else {
-            heading.textContent = "It's a Draw!";
-            }
